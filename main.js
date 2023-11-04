11804195733d8821ae6bca5a706b91e2aa3d82fa
BerkOzerr <146490036+BerkOzerr@users.noreply.github.com>
2023-11-04 17:14:23 +0300

    final word counter me

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-const btn = document.getElementById("btn-cw");
 const words = document.getElementById("words-counter");
 const wordCount = document.getElementById("words");
+const spaceCount = document.getElementById("space");
+const letterCount = document.getElementById("letter");
 
-btn.addEventListener("click", wordMaker);
+words.addEventListener("input", wordMaker);
 
-function wordMaker() {
-  const wordsV = words.value;
-  let wordsTrimmed = wordsV.replace(/\s+/g, " ").trim(); //space handler
-  console.log(wordsTrimmed);
+function wordMaker(e) {
+  let wordsTrimmed = words.value.replace(/\s+/g, " ").trim(); //space handler
+  let spaceTrimmed = words.value.replace(/\s/g, "#");
   let splitWords = wordsTrimmed.split(" ");
-  console.log(splitWords);
   let numberofWord = splitWords.length;
+  let numberofSpace = 0;
+  let numberofletter = wordsTrimmed.length;
+
+  for (let index = 0; index < spaceTrimmed.length; index++) {
+    if (spaceTrimmed[index] === "#") {
+      numberofSpace++;
+    }
+  }
 
   if (splitWords[0] === "") {
     numberofWord = 0;
   }
+
   wordCount.innerHTML = numberofWord;
+  spaceCount.innerHTML = numberofSpace;
+  letterCount.innerHTML = numberofletter;
 }
 
 // var str = '  A B  C   D EF ';
